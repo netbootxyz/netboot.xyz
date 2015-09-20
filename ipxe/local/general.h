2c8dad064fc29f40e9a179556a0e988de2da9c2b
Antony Messerli <antony@mes.ser.li>
2015-09-20 14:31:41 -0500

    Adding v6 and vlan support

--- ipxe/local/general.h
+++ ipxe/local/general.h
@@ -1,2 +1,4 @@
-#define IMAGE_COMBOOT
-#define DOWNLOAD_PROTO_HTTPS
+#define DOWNLOAD_PROTO_HTTPS  /* Secure Hypertext Transfer Protocol */      
+#define IMAGE_COMBOOT         /* COMBOOT */
+#define NET_PROTO_IPV6        /* IPv6 protocol */
+#define VLAN_CMD              /* VLAN commands */
