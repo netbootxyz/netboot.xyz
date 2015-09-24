d5dd98c151d7f7dfab2c0f46852d4935b00d2400
Antony Messerli <antony@mes.ser.li>
2015-09-24 14:44:36 -0500

    Generate sha256-checksums for iPXE bootloaders

--- script/prep-release.sh
+++ script/prep-release.sh
@@ -12,6 +12,11 @@ cp ipxe/local/* ipxe_build/src/config/local/
 
 # build iPXE disks
 cd ipxe_build/src
+
+# get current iPXE hash
+IPXE_HASH=`git log -n 1 --pretty=format:"%H"`
+
+# generate iPXE disks
 for ipxe_config in `ls ../../ipxe/disks/`
 do 
   make EMBED=../../ipxe/disks/$ipxe_config
@@ -22,6 +27,19 @@ do
   mv bin/undionly.kpxe ../../build/ipxe/$ipxe_config-undionly.kpxe
 done
 cd ../..
+
+# generate header for sha256-checksums file
+cat > "build/ipxe/netboot.xyz-sha256-checksums.txt" <<EOF
+# netboot.xyz bootloaders generated from https://github.com/ipxe/ipxe
+# Git Hash: $IPXE_HASH
+
+EOF
+
+# generate sha256sums for iPXE disks
+for ipxe_disk in `ls build/ipxe/`
+do
+  sha256sum $ipxe_disk >> build/ipxe/netboot.xyz-sha256-checksums.txt
+done
 
 # copy iPXE src code into build directory
 cp -R src/* build/
