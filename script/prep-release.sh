5d26fb330549755a6b16588e8dbac0404a44f998
Antony Messerli <antony@mes.ser.li>
2015-12-29 08:30:45 -0600

    Remove index.html before deploy

--- script/prep-release.sh
+++ script/prep-release.sh
@@ -81,5 +81,8 @@ done
 mv ../netboot.xyz-sha256-checksums.txt .
 cd ../..
 
+# delete index.html so that we don't overwrite existing content type
+rm src/index.html
+
 # copy iPXE src code into build directory
 cp -R src/* build/
