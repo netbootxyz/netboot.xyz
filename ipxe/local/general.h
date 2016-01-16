c162ad02a9697444ff4ac913d9d6182743d5ca19
Antony Messerli <antony@mes.ser.li>
2016-01-15 21:09:00 -0600

    Adding image crypto digest commands to iPXE images

--- ipxe/local/general.h
+++ ipxe/local/general.h
@@ -1,3 +1,4 @@
+#define DIGEST_CMD            /* Image crypto digest commands */
 #define DOWNLOAD_PROTO_HTTPS  /* Secure Hypertext Transfer Protocol */      
 #define IMAGE_COMBOOT         /* COMBOOT */
 #define NET_PROTO_IPV6        /* IPv6 protocol */
