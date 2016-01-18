25910be18da219c4a5a6cd292e25eb8b1dd147ed
Antony Messerli <amesserl@rackspace.com>
2016-01-18 09:53:24 -0600

    Add support for image trust commands

--- ipxe/local/general.h
+++ ipxe/local/general.h
@@ -1,5 +1,6 @@
 #define DIGEST_CMD            /* Image crypto digest commands */
 #define DOWNLOAD_PROTO_HTTPS  /* Secure Hypertext Transfer Protocol */      
 #define IMAGE_COMBOOT         /* COMBOOT */
+#define IMAGE_TRUST_CMD	      /* Image trust management commands */
 #define NET_PROTO_IPV6        /* IPv6 protocol */
 #define VLAN_CMD              /* VLAN commands */
