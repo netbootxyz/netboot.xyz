4354fcc3a7f7d7f031a42e35bb2550f02c4b487b
Antony Messerli <antony@mes.ser.li>
2016-02-27 17:18:44 -0600

    Testing out new version of bootdisk to consolidate other disks

--- ipxe/local/general.h
+++ ipxe/local/general.h
@@ -3,4 +3,5 @@
 #define IMAGE_COMBOOT         /* COMBOOT */
 #define IMAGE_TRUST_CMD	      /* Image trust management commands */
 #define NET_PROTO_IPV6        /* IPv6 protocol */
+#define REBOOT_CMD            /* Reboot command */
 #define VLAN_CMD              /* VLAN commands */
